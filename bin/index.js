ccbbcf604b0e78c224c334e0ab5e033227c23981
Gim <jjqian@creams.io>
2020-10-04 18:32:18 +0800

    feat: 添加输出选择方式

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 
 const chalk = require('chalk');
 const fs = require('fs');
+const inquirer = require('inquirer');
 
 let defaultSettings = require('../lib/core/defaultSettings/index');
 const {combineObj} = require('../lib/utils');
@@ -19,23 +20,34 @@ if (!rootPath) {
    process.exit(1);
 }
 
-const configPath = `${rootPath}/match-file.config.js`;
-try {
-    // 配置文件存在 读取配置文件内容并且执行
-    fs.accessSync(configPath);
-    // 获取文件内容， 并且传入主程序
-    combineObj(defaultSettings.getInstance(), require(configPath));
-} catch(err) {
-    log(chalk.red(`配置文件 ${chalk.green('match-file.config.js')} 不存在, 按默认配置解析`));
-}
-
-// 异常捕获
-try {
+inquirer
+  .prompt([
+    {
+        type: 'list',
+        choices: [
+            '打印结果到控制台',
+            '输出结果到文件'
+        ],
+        name: 'outputType',
+        message: '请选择将解析结果进行输出的方式',
+    }
+  ])
+  .then(answers => {
+    defaultSettings.getInstance().inquirerOptions = {...defaultSettings.inquirerOptions, ...{outputType: answers.outputType === '输出结果到文件'}};
+    const configPath = `${rootPath}/match-file.config.js`;
+    try {
+        // 配置文件存在 读取配置文件内容并且执行
+        fs.accessSync(configPath);
+        // 获取文件内容， 并且传入主程序
+        combineObj(defaultSettings.getInstance(), require(configPath));
+    } catch(err) {
+        log(chalk.red(`配置文件 ${chalk.green('match-file.config.js')} 不存在, 按默认配置解析`));
+    }
     searcher();
-} catch(err) {
+  })
+  .catch(err => {
     log(`${chalk.red(err)}`);
-}
-// searcher();
+});
 
 
 
