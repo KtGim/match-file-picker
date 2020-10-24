b80b63c4f50a9790e4f1fd1de2bfa9ea7252aa7f
Gim <jjqian@creams.io>
2020-10-24 22:11:25 +0800

    feat: 添加 ts 解析筛选项

--- bin/index.js
+++ bin/index.js
@@ -13,7 +13,6 @@ const searcher = require('../lib/index');
 const {log} = console;
 
 const rootPath = process.env.PWD;
-
 if (!rootPath) {
    log((chalk.red('获取不到当前执行根目录， 请查看当前执行目录环境')));
    // 退出环境
@@ -22,6 +21,12 @@ if (!rootPath) {
 
 inquirer
   .prompt([
+    {
+      type: 'confirm',
+      name: 'hasTs',
+      message: '项目中是否包含 ts(typescript) 文件 解析',
+      default: false
+    },
     {
         type: 'list',
         choices: [
@@ -45,10 +50,15 @@ inquirer
       ...{autoDelete: answers.autoDelete},
     };
     const configPath = `${rootPath}/match-file.config.js`;
+    const settings = defaultSettings.getInstance();
+    // 如果项目中包含 ts 文件，那么需要添加 ts 解析包， 否则会报错
+    if (answers.hasTs) {
+      settings.babelSetting.presets.push('@babel/preset-typescript');
+    }
     // 配置文件存在 读取配置文件内容并且执行
     if (fs.existsSync(configPath)) {
       // 获取文件内容， 并且传入主程序
-      combineObj(defaultSettings.getInstance(), require(configPath));
+      combineObj(settings, require(configPath));
     } else {
       log(chalk.red(`配置文件 ${chalk.green('match-file.config.js')} 不存在, 按默认配置解析`));
     }
@@ -56,7 +66,8 @@ inquirer
   })
   .catch(err => {
     log(`${chalk.red(err)}`);
-});
+    process.exit(-1);
+  });
 
 
 
